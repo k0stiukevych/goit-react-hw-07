20c61501c185248fc5017797457b113468ce5be5
k0stiukevych <153552996+k0stiukevych@users.noreply.github.com>
2024-08-06 23:22:45 +0300

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,35 @@
+import css from "./Contact.module.css";
+import { deleteContact } from "../../redux/contactsSlice";
+import { useDispatch } from "react-redux";
+import { IoIosContact } from "react-icons/io";
+import { BsFillTelephoneForwardFill } from "react-icons/bs";
+
+
+export default function Contact({ contact: { name, number, id } }) {
+  const dispatch = useDispatch();
+  const handleDelete = (id) => {
+    dispatch(deleteContact(id));
+  };
+
+  return (
+    <div className={css.box}>
+      <div className={css.boxText}>
+        <p className={css.text}>
+          <IoIosContact className={css.contactIcon} />
+          {name}
+        </p>
+        <p className={css.text}>
+          <BsFillTelephoneForwardFill className={css.contactIconPhone} />
+          {number}
+        </p>
+      </div>
+      <button
+        className={css.btn}
+        type="button"
+        onClick={() => handleDelete(id)}
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
